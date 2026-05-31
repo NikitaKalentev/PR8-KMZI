90f5624a1a2af1e6471d46c0f74600b51eb50a7e
NickWest <157978578+NikitaKalentev@users.noreply.github.com>
2026-05-31 19:48:12 +0300

    Update modular.py

--- modular.py
+++ modular.py
@@ -1,12 +1,12 @@
 # modular.py
-def egcd(a, b):
+def egcd(a: int, b: int):
     """Расширенный алгоритм Евклида: возвращает (gcd, x, y): a*x + b*y = gcd."""
     if b == 0:
         return a, 1, 0
     g, x1, y1 = egcd(b, a % b)
     return g, y1, x1 - (a // b) * y1
 
-def modinv(a, n):
+def modinv(a: int, n: int) -> int:
     """Возвращает x, такой что a*x ≡ 1 (mod n). n > 0, a и n взаимно просты."""
     g, x, _ = egcd(a, n)
     if g != 1:
